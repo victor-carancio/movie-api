b798a1e02047559d18702ad010d44788fa9084e0
victorcarancio <victor.carancio@gmail.com>
2023-09-12 19:09:44 -0300

    Se modifica ruta de swagger

--- src/app.js
+++ src/app.js
@@ -35,11 +35,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get("/", (req, res) => {
-  res.send('<h1>Movies API</h1><a href="/api-docs">Documentation</a>');
-});
+// app.get("/", (req, res) => {
+//   res.send('<h1>Movies API</h1><a href="/api-docs">Documentation</a>');
+// });
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //routes
 app.use("/api/v1/movie", moviesRoute);
